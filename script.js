551710952a942ffd2f45e20931b775f98a51c81f
Anonimity <surajangom9@gmail.com>
2025-08-01 06:42:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,4 @@ function handleNoClick() {
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
-} 
-function handleYesClick() {
-    // Optional: Redirect to yes_page
-    window.location.href = "yes_page.html";
+}
